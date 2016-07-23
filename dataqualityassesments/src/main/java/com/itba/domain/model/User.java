9e4c612bafa2a97a6bf81c4af3b82e3cc62763e9
Facundo Alderete <facualderete@gmail.com>
2016-07-23 15:00:51 -0300

    User

---
+++ dataqualityassesments/src/main/java/com/itba/domain/model/User.java
@@ -0,0 +1,11 @@
+package com.itba.domain.model;
+
+import com.itba.domain.PersistentEntity;
+
+import javax.persistence.Entity;
+
+@Entity
+//@Table(name = "users")
+public class User extends PersistentEntity {
+
+}
