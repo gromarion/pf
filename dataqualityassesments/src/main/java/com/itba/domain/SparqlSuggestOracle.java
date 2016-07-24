e6583c43ece8a0ba3a8e27130420fd97a215256c
German Rodrigo Romarion <grromarion90@gmail.com>
2016-07-24 17:05:11 -0300

    Further attempt on suggestion engine

--- dataqualityassesments/src/main/java/com/itba/domain/SparqlSuggestOracle.java
+++ dataqualityassesments/src/main/java/com/itba/domain/SparqlSuggestOracle.java
@@ -2,15 +2,12 @@ package com.itba.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.http.client.RequestException;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.SuggestOracle;
-import com.google.gwt.user.client.ui.SuggestOracle.Response;
 import com.google.gwt.user.client.ui.SuggestOracle.Suggestion;
 
 import sparql.Endpoint;
@@ -18,15 +15,17 @@ import sparql.JsonSparqlResult;
 import sparql.ResultItem;
 
 public class SparqlSuggestOracle {
+	
+	private static boolean responseReceived;
 
-	public void requestSuggestions(String text) {
-        // TODO Auto-generated method stub
+	public static List<Suggestion> requestSuggestions(String text) {
 		Endpoint endpoint = new Endpoint(
             0,
             "http://live.dbpedia.org/sparql",
             "http://dbpedia.org",
             "live.dbpedia.org/sparql"
         );
+		final List<Suggestion> suggestions = new ArrayList<Suggestion>();
         if (text.length() > 2) {
             try {
                 String query = endpoint.getQueryforAutocomplete(text);
@@ -34,18 +33,15 @@ public class SparqlSuggestOracle {
 
                 RequestBuilder rb = new RequestBuilder(RequestBuilder.GET, queryURL);
                 rb.setCallback(new com.google.gwt.http.client.RequestCallback() {
-
                     public void onResponseReceived(com.google.gwt.http.client.Request req, com.google.gwt.http.client.Response res) {
                         try {
                             JsonSparqlResult result = new JsonSparqlResult(res.getText());
-                            Collection<Suggestion> suggestions = new ArrayList<Suggestion>();
                             for (List<ResultItem> i : result.data) {
                                 if (i.size() == 1) {
                                     suggestions.add(new SparqlSuggestItem(i.get(0).value));
                                 }
                             }
-
-//                            callback.onSuggestionsReady(request, new Response(suggestions));
+                            responseReceived = true;
                         } catch (Exception e) {
                             e.printStackTrace();
                             Window.alert("Error communicating with SPARQL Endpoint!");
@@ -53,22 +49,21 @@ public class SparqlSuggestOracle {
                     }
 
                     public void onError(com.google.gwt.http.client.Request request, Throwable exception) {
-
                     }
                 });
                 rb.send();
+                while(!responseReceived) {
+                }
+                return suggestions;
             } catch (RequestException e) {
                 Window.alert("Error occurred" + e.getMessage());
+                return suggestions;
             }
-
-        } else {
-            Response response = new Response(
-                    Collections.<Suggestion>emptyList());
-//            callback.onSuggestionsReady(request, response);
         }
+        return suggestions;
     }
 	
-	public class SparqlSuggestItem implements SuggestOracle.Suggestion, Serializable {
+	public static class SparqlSuggestItem implements SuggestOracle.Suggestion, Serializable {
         private static final long serialVersionUID = 1L;
         public String uri;
 
