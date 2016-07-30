8c2ea0b068f649d296570481e6ccf0a747ecca33
Facundo Alderete <facualderete@gmail.com>
2016-07-30 15:59:31 -0300

    Error repo

--- dataqualityassesments/src/main/java/com/itba/web/panel/ErrorPanel.java
+++ dataqualityassesments/src/main/java/com/itba/web/panel/ErrorPanel.java
@@ -1,15 +1,48 @@
 package com.itba.web.panel;
 
+import com.itba.domain.ErrorRepo;
+import com.itba.domain.model.Error;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import java.util.List;
 
 @SuppressWarnings("serial")
 public class ErrorPanel extends Panel {
+
+	@SpringBean
+	private ErrorRepo errorRepo;
 
 	// este panel contiene todos los errores. Cuando se abre, se le setea el predicate y el objetc
 	// tiene además un botón para cerrarlo y uno para confirmar la operación
 	public ErrorPanel(String id) {
 		super(id);
-		// TODO Auto-generated constructor stub
+
+		List<Error> errorList = errorRepo.getAll();
+		add(new ListView<Error>("resultItemList", errorList) {
+			@Override
+			protected void populateItem(ListItem<Error> listItem) {
+				Error error = listItem.getModelObject();
+
+
+
+
+//				listItem.add(new Label("predicate", resultItem.get(0)));
+//				listItem.add(new Label("object", resultItem.get(1)));
+//				listItem.add(new AjaxLink<Void>("showModalLink") {
+//					@Override
+//					public void onClick(AjaxRequestTarget target)
+//					{
+//						errorModalWindow.show(target);
+//					}
+//				});
+			}
+		});
+
+
+
 		
 		
 		
--- dataqualityassesments/src/test/resources/db/postgresql-create-tables.sql
+++ dataqualityassesments/src/test/resources/db/postgresql-create-tables.sql
@@ -1,35 +1,5 @@
--- MySQL dump 10.13  Distrib 5.5.28, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: evaluation
--- ------------------------------------------------------
--- Server version	5.5.28-0ubuntu0.12.04.2
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table users
---
---
---CREATE DATABASE evaluation
---  WITH OWNER = finalproject
---       ENCODING = 'UTF8'
---       TABLESPACE = pg_default
---       LC_COLLATE = 'C'
---       LC_CTYPE = 'C'
---       CONNECTION LIMIT = -1;
-
 DROP TABLE IF EXISTS users;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE users (
   id SERIAL,
   googleid varchar(64) NOT NULL,
@@ -91,7 +61,7 @@ CREATE TABLE classes (
   cname varchar(256) NOT NULL,
   cparent bigint NOT NULL,
   count_cache bigint NOT NULL,
-  is_leaf int NOT NULL,
+  is_leaf boolean NOT NULL,
   PRIMARY KEY (id)
 );
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -333,7 +303,7 @@ CREATE TABLE evaluated_resource (
   resource varchar(256) NOT NULL,
   comments varchar(10000) NOT NULL,
   class varchar(256) NOT NULL DEFAULT '',
-  correct int NOT NULL,
+  correct boolean NOT NULL,
   PRIMARY KEY (id)
 );
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -361,21 +331,3 @@ CREATE TABLE evaluated_resource_details (
   comment varchar(10000) DEFAULT NULL,
   PRIMARY KEY (id)
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table evaluated_resource_details
---
-
-/*!40000 ALTER TABLE evaluated_resource_details DISABLE KEYS */;
-/*!40000 ALTER TABLE evaluated_resource_details ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2012-11-13 20:07:33
