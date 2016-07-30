7ffc540cdd0c9fa34cabc85e9f7d22c5122b34df
German Rodrigo Romarion <grromarion90@gmail.com>
2016-07-30 18:00:59 -0300

    Added library to measure server performance

---
+++ dataqualityassesments/src/main/java/com/itba/automatic/ScalabilityChecker.java
@@ -0,0 +1,7 @@
+package com.itba.automatic;
+
+public class ScalabilityChecker {
+	public static boolean check() {
+		return false;
+	} 
+}
--- dataqualityassesments/src/main/java/com/itba/automatic/UriLengthChecker.java
+++ dataqualityassesments/src/main/java/com/itba/automatic/UriLengthChecker.java
@@ -2,9 +2,9 @@ package com.itba.automatic;
 
 public class UriLengthChecker {
 	
-	public int MAX_URI_LENGTH = 2048;
+	public static int MAX_URI_LENGTH = 2048;
 
-	public boolean checker(String uri) {
+	public static boolean checker(String uri) {
 		boolean validUriLength = uri.length() <= MAX_URI_LENGTH;
 		boolean noQueryParams = uri.contains("?");
 		return validUriLength && noQueryParams;
