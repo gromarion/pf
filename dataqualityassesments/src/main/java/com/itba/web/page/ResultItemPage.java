06da01c733ba20200ef6dd05d5c187d1a0d866b9
gromarion <grromarion90@gmail.com>
2017-06-08 21:44:44 -0300

    Added d3.js library to project. Imported from ResultItemPage.java

--- dataqualityassesments/src/main/java/com/itba/web/page/ResultItemPage.java
+++ dataqualityassesments/src/main/java/com/itba/web/page/ResultItemPage.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
@@ -14,8 +16,8 @@ import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
 import org.apache.wicket.spring.injection.annot.SpringBean;
-
 import com.itba.domain.SparqlRequestHandler;
 import com.itba.domain.model.Campaign;
 import com.itba.domain.repository.CampaignRepo;
@@ -40,6 +42,13 @@ public class ResultItemPage extends BasePage {
 	private ErrorRepo errorRepo;
 	@SpringBean
 	private EvaluatedResourceDetailRepo evaluatedResourceDetailRepo;
+	
+	@Override
+	public void renderHead(IHeaderResponse response) {
+	    super.renderHead(response);
+
+	    response.render(JavaScriptHeaderItem.forReference(new JavaScriptResourceReference(ResultItemPage.class, "d3.js")));
+	}
 
 	public ResultItemPage(PageParameters parameters) {
 		final String resource = parameters.get("selection").toString();
