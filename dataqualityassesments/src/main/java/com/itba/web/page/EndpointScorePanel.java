d3b12538cfbd42ddd923f272705514f44aecadf9
gromarion <grromarion90@gmail.com>
2017-06-13 20:49:55 -0300

    Removed hardcoded domain from donut-chart.js. Implemented EndpointScorePanel

---
+++ dataqualityassesments/src/main/java/com/itba/web/page/EndpointScorePanel.java
@@ -0,0 +1,61 @@
+package com.itba.web.page;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
+
+import com.itba.domain.model.EndpointStats;
+import com.itba.domain.repository.EndpointStatsRepo;
+
+public class EndpointScorePanel extends Panel {
+	private static final long serialVersionUID = 1L;
+	
+	private EndpointStatsRepo endpointStatsRepo;
+	private String endpointUrl;
+
+	public EndpointScorePanel(String id, EndpointStatsRepo endpointStatsRepo, String endpointUrl) {
+		super(id);
+		this.endpointStatsRepo = endpointStatsRepo;
+		this.endpointUrl = endpointUrl;
+	}
+	
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		super.renderHead(response);
+		
+		List<EndpointStats> endpointStats = endpointStatsRepo.getAllForEndpoint(endpointUrl);
+		
+		if (endpointStats.isEmpty()) {
+			return;
+		}
+
+		String errorsData = "[";
+		Map<String, Integer> statusCodesAmount = new HashMap<>();
+		for (EndpointStats stats : endpointStats) {
+			if (statusCodesAmount.containsKey(stats.getStatusCode())) {
+				statusCodesAmount.put(stats.getStatusCode(), statusCodesAmount.get(stats.getStatusCode()) + 1);
+			} else {
+				statusCodesAmount.put(stats.getStatusCode(), 1);
+			}
+		}
+
+		for (String statusCode : statusCodesAmount.keySet()) {
+			errorsData += "{'label': '" + statusCode + "', 'value': " + statusCodesAmount.get(statusCode) + "},";
+		}
+
+		errorsData = errorsData.substring(0, errorsData.length() - 2);
+		errorsData += "]";
+		
+		response.render(JavaScriptHeaderItem
+				.forReference(new JavaScriptResourceReference(ResultItemPage.class, "js/d3.min.js")));
+		response.render(JavaScriptHeaderItem
+				.forReference(new JavaScriptResourceReference(ResultItemPage.class, "js/donut-chart.js")));
+		response.render(OnDomReadyHeaderItem.forScript("drawChart(" + errorsData + ");"));
+	}
+}
