4c24c732894748295f9825483da40f47452e332c
gromarion <grromarion90@gmail.com>
2017-09-03 10:25:23 -0300

    Added evaluated resources stats to homepage

--- src/main/java/com/itba/web/page/EndpointScorePanel.java
+++ src/main/java/com/itba/web/page/EndpointScorePanel.java
@@ -6,23 +6,33 @@ import java.util.Map;
 
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
 
 import com.itba.domain.model.EndpointStats;
 import com.itba.domain.model.Error;
+import com.itba.domain.model.EvaluationSession;
+import com.itba.domain.repository.EvaluatedResourceRepo;
 import com.itba.formulae.EndpointQualityFormulae.EndpointScore;
+import com.itba.web.WicketSession;
 import com.itba.web.charts.DonutChartWithLabels;
 import com.itba.web.charts.GaugeChart;
 
 @SuppressWarnings("serial")
 public class EndpointScorePanel extends Panel {
-	private final Map<String, String> errorColors;
-	
-	private EndpointScore endpointScore;
+	private static final int DURATION = 1;
+	private static final String TOTAL_RESOURCES_ID = "totalResources";
+	private static final String ERRORED_RESOURCES_ID = "erroredResources";
+	private static final String CORRECT_RESOURCES_ID = "correctResources";
 
-	public EndpointScorePanel(String id, EndpointScore endpointScore) {
+	private final Map<String, String> errorColors;
+
+	private EndpointScore endpointScore;
+	private EvaluatedResourceRepo evaluatedResourceRepo;
+
+	public EndpointScorePanel(String id, EndpointScore endpointScore, EvaluatedResourceRepo evaluatedResourceRepo) {
 		super(id);
 		this.errorColors = new HashMap<>();
 		errorColors.put("Tipodedatoincorrectamenteextraído", "#FF7777");
@@ -30,7 +40,8 @@ public class EndpointScorePanel extends Panel {
 		errorColors.put("Objetosemánticamenteincorrecto", "#808015");
 		errorColors.put("Enlaceexternoincorrecto", "#6DA398");
 		this.endpointScore = endpointScore;
-		
+		this.evaluatedResourceRepo = evaluatedResourceRepo;
+
 		add(new Label("endpointURL", endpointScore.getEndpointURL()));
 		add(new Label("endpointScore", endpointScore.getScoreString()));
 	}
@@ -41,14 +52,14 @@ public class EndpointScorePanel extends Panel {
 
 		List<EndpointStats> endpointStats = endpointScore.getEndpointStats();
 		Map<Error, Double> errorTypeStats = endpointScore.getErrorTypeStats();
-		
+
 		if (endpointStats.isEmpty() && errorTypeStats.isEmpty()) {
 			return;
 		}
 
 		DonutChartWithLabels endpointStatsChart = new DonutChartWithLabels("endpoint-availability-chart");
 		GaugeChart errorTypeChart = new GaugeChart();
-		
+
 		Map<String, Integer> statusCodesAmount = new HashMap<>();
 		for (EndpointStats stats : endpointStats) {
 			if (statusCodesAmount.containsKey(stats.getStatusCode())) {
@@ -63,13 +74,30 @@ public class EndpointScorePanel extends Panel {
 		}
 		for (Error e : errorTypeStats.keySet()) {
 			String id = e.getName().replaceAll(" ", "");
-			errorTypeChart.appendData(id, errorTypeStats.get(e), errorColors.get(id), errorColors.get(id), errorColors.get(id));
+			errorTypeChart.appendData(id, errorTypeStats.get(e), errorColors.get(id), errorColors.get(id),
+					errorColors.get(id));
 		}
 
-		response.render(JavaScriptHeaderItem.forReference(new JavaScriptResourceReference(ResultItemPage.class, "js/d3.min.js")));
-		response.render(JavaScriptHeaderItem.forReference(new JavaScriptResourceReference(ResultItemPage.class, "js/donut-chart.js")));
-		response.render(JavaScriptHeaderItem.forReference(new JavaScriptResourceReference(ResultItemPage.class, "js/gauge-chart.js")));
+		EvaluationSession session = WicketSession.get().getEvaluationSession().get();
+		int totalResources = evaluatedResourceRepo.getAllForSession(session).size();
+		int erroredResources = evaluatedResourceRepo.getErroredForSession(session).size();
+		int correctResources = totalResources - erroredResources;
+
+		response.render(JavaScriptHeaderItem
+				.forReference(new JavaScriptResourceReference(ResultItemPage.class, "js/d3.min.js")));
+		response.render(JavaScriptHeaderItem
+				.forReference(new JavaScriptResourceReference(ResultItemPage.class, "js/donut-chart.js")));
+		response.render(JavaScriptHeaderItem
+				.forReference(new JavaScriptResourceReference(ResultItemPage.class, "js/gauge-chart.js")));
+		response.render(JavaScriptHeaderItem
+				.forReference(new JavaScriptResourceReference(ResultItemPage.class, "js/count-up.js")));
 		response.render(endpointStatsChart.getRender());
 		response.render(errorTypeChart.getRender());
+		response.render(OnDomReadyHeaderItem.forScript(
+				"(new CountUp('" + TOTAL_RESOURCES_ID + "', 0, " + totalResources + ", 0, " + DURATION + ")).start();"));
+		response.render(OnDomReadyHeaderItem.forScript(
+				"(new CountUp('" + ERRORED_RESOURCES_ID + "', 0, " + erroredResources + ", 0, " + DURATION + ")).start();"));
+		response.render(OnDomReadyHeaderItem.forScript(
+				"(new CountUp('" + CORRECT_RESOURCES_ID + "', 0, " + correctResources + ", 0, " + DURATION + ")).start();"));
 	}
 }
--- src/main/java/com/itba/web/page/HomePage.java
+++ src/main/java/com/itba/web/page/HomePage.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
+import com.itba.domain.repository.EvaluatedResourceRepo;
 import com.itba.formulae.EndpointQualityFormulae;
 import com.itba.formulae.EndpointQualityFormulae.EndpointScore;
 
@@ -13,6 +14,9 @@ public class HomePage extends BasePage {
 	@SpringBean
 	private EndpointQualityFormulae endpointQualityFormulae;
 
+	@SpringBean
+	private EvaluatedResourceRepo evaluatedResourceRepo;
+
 	@Override
 	protected void onInitialize() {
 		super.onInitialize();
@@ -20,8 +24,9 @@ public class HomePage extends BasePage {
 		EndpointScore endpointScore;
 		try {
 			endpointScore = endpointQualityFormulae.getScore();
-			EndpointScorePanel endpointScorePanel = new EndpointScorePanel("endpointScorePanel", endpointScore);
-			
+			EndpointScorePanel endpointScorePanel = new EndpointScorePanel("endpointScorePanel", endpointScore,
+					evaluatedResourceRepo);
+
 			endpointScorePanel.setVisible(!endpointScore.getEndpointStats().isEmpty());
 			add(endpointScorePanel);
 		} catch (IOException e) {
