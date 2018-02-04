a6ed22aecd95198399c0929d5d2e02ac33c0eaf2
gromarion <grromarion90@gmail.com>
2018-02-04 18:24:28 -0300

    Implemented warning message for non author users on selected errors page

--- src/main/java/com/itba/web/page/ErrorSelectionPage.java
+++ src/main/java/com/itba/web/page/ErrorSelectionPage.java
@@ -40,7 +40,7 @@ import utils.URLHelper;
 
 @SuppressWarnings("serial")
 public class ErrorSelectionPage extends BasePage {
-	
+
 	@SpringBean
 	private ManualErrorsFormulae manualErrorsFormulae;
 	@SpringBean
@@ -53,8 +53,10 @@ public class ErrorSelectionPage extends BasePage {
 	private EvaluatedResourceDetailRepo evaluatedResourceDetailRepo;
 
 	private final IModel<Error> errorModel = new EntityModel<Error>(Error.class);
-	private final IModel<EvaluationSession> currentSession = new EntityModel<EvaluationSession>(EvaluationSession.class);
-	private final IModel<EvaluatedResource> evaluatedResource = new EntityModel<EvaluatedResource>(EvaluatedResource.class);
+	private final IModel<EvaluationSession> currentSession = new EntityModel<EvaluationSession>(
+			EvaluationSession.class);
+	private final IModel<EvaluatedResource> evaluatedResource = new EntityModel<EvaluatedResource>(
+			EvaluatedResource.class);
 	private final IModel<String> errorDescription = Model.of();
 	private final IModel<String> errorExample = Model.of();
 	private final IModel<List<Error>> availableErrors = new LoadableDetachableModel<List<Error>>() {
@@ -65,81 +67,83 @@ public class ErrorSelectionPage extends BasePage {
 	};
 
 	public ErrorSelectionPage(PageParameters parameters) {
-    	if (!((WicketSession) getSession()).isSignedIn()) {
-        	setResponsePage(LoginPage.class);
+		if (!((WicketSession) getSession()).isSignedIn()) {
+			setResponsePage(LoginPage.class);
 		}
-    	
-        currentSession.setObject(WicketSession.get().getEvaluationSession().get());
 
-    	final String predicate = parameters.get("predicate").toString();
-    	final String object = parameters.get("object").toString();
-    	final String resource = parameters.get("resource").toString();
-    	if (parameters.getNamedKeys().contains("sessionId")) {
-    		evaluatedResource.setObject(evaluatedResourceRepo.getResourceForSession(evaluationSessionRepo.get(parameters.get("sessionId").toInt()), resource).orNull());
-    	}
-    	
-    	SelectedErrorsTablePanel selectedErrorsTablePanel = new SelectedErrorsTablePanel("selectedErrorsTablePanel", predicate, object, evaluatedResource);
-    	
-    	add(selectedErrorsTablePanel);
-    	IModel<List<EvaluatedResourceDetail>> usedErrorDetails = selectedErrorsTablePanel.getUsedErrorDetails();
-    	selectedErrorsTablePanel.setVisible(usedErrorDetails.getObject().size() > 0);
-    	
-    	for (int i = 0 ; i < usedErrorDetails.getObject().size() ; i++) {
-    		availableErrors.getObject().remove(usedErrorDetails.getObject().get(i).getError());
-    	}
-    	
-        add(new CustomFeedbackPanel("feedbackPanel"));
-    	
-    	if(!StringUtils.containsURL(object)) {
-    		availableErrors.getObject().remove(errorRepo.get(4));
-    	}
-    	
-            	
-        final TextArea<String> comments = new TextArea<String>("comments", Model.of(""));
-        final ExternalLink predicateLink = new ExternalLink("predicateLink", predicate, predicate);
-        final Label objectLabel = new Label("objectLabel", URLHelper.transformURLs(object, false));
-        objectLabel.setEscapeModelStrings(false);
-        final ExternalLink resourceLink = new ExternalLink("resourceLink", resource, resource);
-        final Label errorDescriptionLabel = new Label("errorDescription", errorDescription);
-        final Label errorExampleLabel = new Label("errorExample", errorExample);
-        
-        errorDescriptionLabel.setOutputMarkupId(true);
-        errorExampleLabel.setOutputMarkupId(true);
-        
-        if (availableErrors.getObject().size() > 0) {
-        	errorModel.setObject(availableErrors.getObject().get(0));
-        	errorDescriptionLabel.setDefaultModelObject(errorModel.getObject().getDescription());
-        	errorExampleLabel.setDefaultModelObject(errorModel.getObject().getExample());
-        }
-    	
-    	DropDownChoice<Error> errorListChoice = 
-                new DropDownChoice<Error>("errorList", errorModel,
-                        new LoadableDetachableModel<List<Error>>() {
-                            @Override
-                            protected List<Error> load() { 
-                                return availableErrors.getObject();
-                            }
-                        }
-                    , new ChoiceRenderer<Error>("name")) {
-        };
-        
-        OnChangeAjaxBehavior onChangeAjaxBehavior = new OnChangeAjaxBehavior() {
-            @Override
-            protected void onUpdate(AjaxRequestTarget target) {
-            	errorDescriptionLabel.setDefaultModelObject(errorModel.getObject().getDescription());
-            	errorExampleLabel.setDefaultModelObject(errorModel.getObject().getExample());
-            	target.add(errorDescriptionLabel);
-            	target.add(errorExampleLabel);
-            }
-        };
-        errorListChoice.add(onChangeAjaxBehavior);
-        		
-        Form<ErrorSelectionPage> form = new Form<ErrorSelectionPage>("errorForm",
-                new CompoundPropertyModel<ErrorSelectionPage>(this)) {
+		currentSession.setObject(WicketSession.get().getEvaluationSession().get());
+
+		final String predicate = parameters.get("predicate").toString();
+		final String object = parameters.get("object").toString();
+		final String resource = parameters.get("resource").toString();
+		if (parameters.getNamedKeys().contains("sessionId")) {
+			evaluatedResource.setObject(evaluatedResourceRepo
+					.getResourceForSession(evaluationSessionRepo.get(parameters.get("sessionId").toInt()), resource)
+					.orNull());
+		}
+
+		boolean isAuthor = evaluatedResource.getObject().getSession().getUser().equals(WicketSession.get().getUser());
+		SelectedErrorsTablePanel selectedErrorsTablePanel = new SelectedErrorsTablePanel("selectedErrorsTablePanel",
+				predicate, object, evaluatedResource, isAuthor);
+
+		add(selectedErrorsTablePanel);
+		IModel<List<EvaluatedResourceDetail>> usedErrorDetails = selectedErrorsTablePanel.getUsedErrorDetails();
+		selectedErrorsTablePanel.setVisible(usedErrorDetails.getObject().size() > 0);
+
+		for (int i = 0; i < usedErrorDetails.getObject().size(); i++) {
+			availableErrors.getObject().remove(usedErrorDetails.getObject().get(i).getError());
+		}
+
+		add(new CustomFeedbackPanel("feedbackPanel"));
+
+		if (!StringUtils.containsURL(object)) {
+			availableErrors.getObject().remove(errorRepo.get(4));
+		}
+
+		final TextArea<String> comments = new TextArea<String>("comments", Model.of(""));
+		final ExternalLink predicateLink = new ExternalLink("predicateLink", predicate, predicate);
+		final Label objectLabel = new Label("objectLabel", URLHelper.transformURLs(object, false));
+		objectLabel.setEscapeModelStrings(false);
+		final ExternalLink resourceLink = new ExternalLink("resourceLink", resource, resource);
+		final Label errorDescriptionLabel = new Label("errorDescription", errorDescription);
+		final Label errorExampleLabel = new Label("errorExample", errorExample);
+
+		errorDescriptionLabel.setOutputMarkupId(true);
+		errorExampleLabel.setOutputMarkupId(true);
+
+		if (availableErrors.getObject().size() > 0) {
+			errorModel.setObject(availableErrors.getObject().get(0));
+			errorDescriptionLabel.setDefaultModelObject(errorModel.getObject().getDescription());
+			errorExampleLabel.setDefaultModelObject(errorModel.getObject().getExample());
+		}
+
+		DropDownChoice<Error> errorListChoice = new DropDownChoice<Error>("errorList", errorModel,
+				new LoadableDetachableModel<List<Error>>() {
+					@Override
+					protected List<Error> load() {
+						return availableErrors.getObject();
+					}
+				}, new ChoiceRenderer<Error>("name")) {
+		};
+
+		OnChangeAjaxBehavior onChangeAjaxBehavior = new OnChangeAjaxBehavior() {
 			@Override
-            protected void onSubmit() {
-				
-		        Optional<EvaluatedResource> evaluatedResource = evaluatedResourceRepo.getResourceForSession(currentSession.getObject(), resource);
+			protected void onUpdate(AjaxRequestTarget target) {
+				errorDescriptionLabel.setDefaultModelObject(errorModel.getObject().getDescription());
+				errorExampleLabel.setDefaultModelObject(errorModel.getObject().getExample());
+				target.add(errorDescriptionLabel);
+				target.add(errorExampleLabel);
+			}
+		};
+		errorListChoice.add(onChangeAjaxBehavior);
+
+		Form<ErrorSelectionPage> form = new Form<ErrorSelectionPage>("errorForm",
+				new CompoundPropertyModel<ErrorSelectionPage>(this)) {
+			@Override
+			protected void onSubmit() {
+
+				Optional<EvaluatedResource> evaluatedResource = evaluatedResourceRepo
+						.getResourceForSession(currentSession.getObject(), resource);
 				if (!evaluatedResource.isPresent()) {
 					evaluatedResource = Optional.of(new EvaluatedResource(currentSession.getObject(), resource));
 					evaluatedResourceRepo.save(evaluatedResource.get());
@@ -148,57 +152,61 @@ public class ErrorSelectionPage extends BasePage {
 					r.setTimestamp(System.currentTimeMillis());
 					evaluatedResourceRepo.save(r);
 				}
-				
+
 				evaluatedResource.get().setCorrect(false);
-				
-				EvaluatedResourceDetail detail = new EvaluatedResourceDetail(evaluatedResource.get(), errorModel.getObject(), predicate, object);
+
+				EvaluatedResourceDetail detail = new EvaluatedResourceDetail(evaluatedResource.get(),
+						errorModel.getObject(), predicate, object);
 				detail.setComment(comments.getValue());
 				evaluatedResource.get().getDetails().add(detail);
-				
+
 				try {
-					Score score = manualErrorsFormulae.compute(resource, Optional.of(evaluatedResource.get().getSession()));
-					evaluatedResource.get().setScore(new BigDecimal(score.getScore()).setScale(3, BigDecimal.ROUND_HALF_EVEN));
+					Score score = manualErrorsFormulae.compute(resource,
+							Optional.of(evaluatedResource.get().getSession()));
+					evaluatedResource.get()
+							.setScore(new BigDecimal(score.getScore()).setScale(3, BigDecimal.ROUND_HALF_EVEN));
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				
+
 				PageParameters parameters = new PageParameters();
-                parameters.add("predicate", predicate);
-                parameters.add("object", object);
-                parameters.add("resource", resource);
-                parameters.add("sessionId", evaluatedResource.get().getSession().getId());
-                setResponsePage(ErrorSelectionPage.class, parameters);
-            }
-        };
-        
-        form.add(comments);
-        form.add(errorListChoice);
-        form.add(errorDescriptionLabel);
-        form.add(resourceLink);
-        form.add(objectLabel);
-        form.add(predicateLink);
-        form.add(errorExampleLabel);
-        
-        Button submit = new Button("submit");
-        if (availableErrors.getObject().size() == 0) submit.setVisible(false);
-        form.add(submit);
-        
-        Link<Void> backButton = new Link<Void>("back") {
+				parameters.add("predicate", predicate);
+				parameters.add("object", object);
+				parameters.add("resource", resource);
+				parameters.add("sessionId", evaluatedResource.get().getSession().getId());
+				setResponsePage(ErrorSelectionPage.class, parameters);
+			}
+		};
+
+		form.add(comments);
+		form.add(errorListChoice);
+		form.add(errorDescriptionLabel);
+		form.add(resourceLink);
+		form.add(objectLabel);
+		form.add(predicateLink);
+		form.add(errorExampleLabel);
+
+		Button submit = new Button("submit");
+		if (availableErrors.getObject().size() == 0)
+			submit.setVisible(false);
+		form.add(submit);
+
+		Link<Void> backButton = new Link<Void>("back") {
 			@Override
 			public void onClick() {
 				PageParameters parameters = new PageParameters();
 				String resourceURL = resource;
-                parameters.add("selection", resourceURL);
-                setResponsePage(ResultItemPage.class, parameters);
+				parameters.add("selection", resourceURL);
+				setResponsePage(ResultItemPage.class, parameters);
 			}
 		};
 		add(backButton);
-        
-		boolean showForm = availableErrors.getObject().size() > 0 &&
-				(evaluatedResource.getObject() == null || (evaluatedResource.getObject().getSession().getUser().equals(WicketSession.get().getUser())));
-		
-        add(form.setVisible(showForm));
-    }
+
+		boolean showForm = availableErrors.getObject().size() > 0
+				&& (evaluatedResource.getObject() == null || isAuthor);
+
+		add(form.setVisible(showForm));
+	}
 
 	@Override
 	protected void onDetach() {
--- src/main/java/com/itba/web/page/SelectedErrorsTablePanel.java
+++ src/main/java/com/itba/web/page/SelectedErrorsTablePanel.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
@@ -30,34 +31,42 @@ import lib.Score;
 
 @SuppressWarnings("serial")
 public class SelectedErrorsTablePanel extends Panel {
-	
+
 	@SpringBean
 	private EvaluatedResourceDetailRepo evaluatedResourceDetailRepo;
 	@SpringBean
 	private ErrorRepo errorRepo;
 	@SpringBean
 	private ManualErrorsFormulae manualErrorsFormulae;
-	
+
 	private final IModel<List<EvaluatedResourceDetail>> usedErrorDetails;
 
-	public SelectedErrorsTablePanel(String id, final String predicate, final String object, final IModel<EvaluatedResource> resource) {
+	public SelectedErrorsTablePanel(String id, final String predicate, final String object,
+			final IModel<EvaluatedResource> resource, boolean isAuthor) {
 		super(id);
-		
+
 		this.usedErrorDetails = new LoadableDetachableModel<List<EvaluatedResourceDetail>>() {
-    	    @Override
-    	    protected List<EvaluatedResourceDetail> load() {
-    	        return resource.getObject() == null ? Lists.newArrayList() : evaluatedResourceDetailRepo.getPreviousErrors(resource.getObject(), predicate, object);
-    	    }
-    	};
-    	
-    	final Label errorActionsLabel = new Label("errorActionsLabel", getString("errorActionsLabel"));
-    	
-    	add(new ListView<EvaluatedResourceDetail>("usedErrorDetails", usedErrorDetails) {
+			@Override
+			protected List<EvaluatedResourceDetail> load() {
+				return resource.getObject() == null ? Lists.newArrayList()
+						: evaluatedResourceDetailRepo.getPreviousErrors(resource.getObject(), predicate, object);
+			}
+		};
+		
+		WebMarkupContainer alertContainer = new WebMarkupContainer("alertContainer");
+		alertContainer.add(new Label("readOnlyWarning", getString("readOnlyWarning")));
+		alertContainer.setVisible(!isAuthor);
+		add(alertContainer);
+
+		final Label errorActionsLabel = new Label("errorActionsLabel", getString("errorActionsLabel"));
+
+		add(new ListView<EvaluatedResourceDetail>("usedErrorDetails", usedErrorDetails) {
 			@Override
 			protected void populateItem(final ListItem<EvaluatedResourceDetail> errorDetail) {
-				
-				boolean isAuthorUser = WicketSession.get().getUser().equals(resource.getObject().getSession().getUser());
-				
+
+				boolean isAuthorUser = WicketSession.get().getUser()
+						.equals(resource.getObject().getSession().getUser());
+
 				final PageParameters refreshParameters = new PageParameters();
 				refreshParameters.add("predicate", predicate);
 				refreshParameters.add("object", object);
@@ -72,10 +81,11 @@ public class SelectedErrorsTablePanel extends Panel {
 							EvaluatedResource resource = errorDetail.getModelObject().getResource();
 							resource.getDetails().remove(errorDetail.getModelObject());
 							evaluatedResourceDetailRepo.delete(errorDetail.getModelObject());
-							Score score = manualErrorsFormulae.compute(resource.getResource(), Optional.of(resource.getSession()));
+							Score score = manualErrorsFormulae.compute(resource.getResource(),
+									Optional.of(resource.getSession()));
 							resource.setScore(new BigDecimal(score.getScore()).setScale(3, RoundingMode.HALF_EVEN));
 							setResponsePage(ErrorSelectionPage.class, refreshParameters);
-						} catch(Exception e) {
+						} catch (Exception e) {
 							// TODO: do nothing for now...
 						}
 					}
@@ -85,17 +95,19 @@ public class SelectedErrorsTablePanel extends Panel {
 					public void onClick(AjaxRequestTarget target) {
 						// TODO: do nothing for now...
 					}
-				}.setVisible(isAuthorUser).add(new AttributeModifier("data-target", "#modal" + errorDetail.getModelObject().getId())));
-				errorDetail.add(new EditErrorCommentModal("editCommentModal", "Editar comentario", errorDetail.getModel(), refreshParameters));
+				}.setVisible(isAuthorUser)
+						.add(new AttributeModifier("data-target", "#modal" + errorDetail.getModelObject().getId())));
+				errorDetail.add(new EditErrorCommentModal("editCommentModal", "Editar comentario",
+						errorDetail.getModel(), refreshParameters));
 				errorActionsLabel.setVisible(isAuthorUser);
 			}
 		}.setVisible(usedErrorDetails.getObject().size() > 0));
-    	
-    	add(errorActionsLabel);
+
+		add(errorActionsLabel);
 	}
-	
+
 	public IModel<List<EvaluatedResourceDetail>> getUsedErrorDetails() {
 		return usedErrorDetails;
 	}
-	
+
 }
